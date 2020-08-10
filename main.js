2c465b3ff57408e59d4608b9a159ef4b1da76c18
BetoSandoval <alberto.sa1391@gmail.com>
2020-08-10 15:20:53 -0500

    funcion play y pause con js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var video = document.getElementById('video'),
     btnPlay = document.getElementById('play'),
-    btnPause = document.getElementById('play');
+    btnPause = document.getElementById('pause');
 
     btnPlay.addEventListener('click',function() {
         video.play();
-    })
+    });
+
+    btnPause.addEventListener('click', function() {
+        video.pause();
+    });
