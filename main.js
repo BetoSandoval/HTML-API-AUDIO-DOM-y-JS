ecf6e023c2decef72cfe3560671d0faa7aafd59b
BetoSandoval <alberto.sa1391@gmail.com>
2020-08-10 15:09:29 -0500

    EventListener play video

---
+++ main.js
@@ -0,0 +1,7 @@
+var video = document.getElementById('video'),
+    btnPlay = document.getElementById('play'),
+    btnPause = document.getElementById('play');
+
+    btnPlay.addEventListener('click',function() {
+        video.play();
+    })
